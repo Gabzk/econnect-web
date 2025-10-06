cf69fe96878e561aba1b307286084972bb72dad6
Gabriel Alves <gabrielrodriguesalves693@gmail.com>
2025-10-06 18:28:29 -0300

    feat: adiciona componente HomeComponent e integra destaques

---
+++ frontend/src/components/homeComponent.tsx
@@ -0,0 +1,15 @@
+import HighlightComponent from "./highlightComponent";
+
+export default function HomeComponent() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-emerald-700 text-center mt-12">
+        Econnect
+      </h1>
+
+      <HighlightComponent title="Notícias mais curtidas" />
+
+      <HighlightComponent title="Últimas Notícias" />
+    </div>
+  );
+}
