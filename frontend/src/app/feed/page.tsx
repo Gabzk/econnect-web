b5ca30c54d93c6f6f2a821214e1bc3ca3f4283c7
Gabriel Alves <gabrielrodriguesalves693@gmail.com>
2025-12-10 14:50:17 -0300

    feat(frontend): encapsula rotas feed e hottest com a protectedRoute que já estava feito mas não foi aplicado.
    também apagar navBarComponent obsoleto

--- frontend/src/app/feed/page.tsx
+++ frontend/src/app/feed/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import FeedComponent from "@/components/feedComponent";
-import NavBarComponent from "@/components/navBarComponentClient";
+import NavBarComponent from "@/components/navBarComponent";
+import ProtectedRoute from "@/components/protectedRoute";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function FeedPage() {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  console.log(isAuthenticated);
   return (
     <div>
       <NavBarComponent />
-      <FeedComponent feedType="latest" />
+      <ProtectedRoute showLoginPrompt={true}>
+        <FeedComponent feedType="latest"/>
+      </ProtectedRoute>
     </div>
   );
 }
